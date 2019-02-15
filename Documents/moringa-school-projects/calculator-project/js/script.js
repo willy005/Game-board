aa54fbc411eb1bc2d734524216cb38ade858bdd2
nillyw31@gmail.com <nillyw31@gmail.com>
2019-02-15 09:48:06 +0300

    Add index.html

---
+++ Documents/moringa-school-projects/calculator-project/js/script.js
@@ -0,0 +1,93 @@
+// function giveanswer(){
+//
+// }
+//     Test.
+// var add = function(number1, number2){
+//   return number1 +number2;
+// }
+// var number1 =parseInt(prompt("Enter a number:"))/
+// var number2 =parseInt(prompt("Enter another number:"))
+//
+// var result = add(number1, number2);
+// alert(result);
+//
+//     About Temperature.
+// var temperature = 30;
+// undefined
+// if (temperature<20) {
+// console.log("Dont forget a jacket");
+// }
+// else {
+//   console.log("keep cool today");
+// }
+//     About odd and even numbers.
+// var number = parseInt(prompt("enter a number"));
+// if (number % 2 === 0){
+//   console.log("number is even");
+// }
+// else {
+//   console.log("number is odd");
+// }
+
+//     About Coffee.
+// var beverage = prompt("choose coffee or tea");
+//
+// if (beverage === "tea"){
+//   console.log("enjoy your tea");
+// } else if (beverage === "coffee") {
+//   console.log("enjoy your coffee");
+// } else {
+//   console.log("jinga wewe");
+// }
+//   About Science.
+var science = prompt("What subject would you choose?");
+if (science === "Biology") {
+  console.log("Learn Biology");
+}
+else if (science === "Physics") {
+  console.log("Learn Physics");
+}
+
+else if (science === "chemistry") {
+    console.log("learn chemistry");
+}
+else {
+  console.log("Go home!");
+}
+
+ // About Grades.
+// var grade = parseInt(prompt("Display grades"));
+// if (grade <=20) {
+//   console.log("grade E");
+//   alert("You have an E!")
+// }
+// else if (grade >20 && grade<40) {
+//   console.log("grade D");
+//   prompt("You have a D")
+// }
+// else if (grade >39 && grade <60) {
+//   console.log("grade C");
+// }
+// else if (grade >59 && grade <80) {
+//   console.log("grade B");
+// }
+// else if (grade >79 && grade <100 ) {
+//   console.log("grade A");
+// }
+//     // About Pet!
+// var pet = parseInt(prompt("Should I get you an introverted or extroverted pet?"));
+//   if (pet === "introverted") {
+//     console.log("I will get you an introverted pet");
+//   }
+//   else if (pet ==="extroverted") {
+//       console.log("I will get you an extroverted pet");
+//   }
+//   else {
+//     console.log("Dont be stupid choose one.");
+//   }
+
+
+
+    // Arrays!
+    // var fruits = ["apple", "orange", "passion fruit", "kiwi"];
+    // fruits.pop();"kiwi"
